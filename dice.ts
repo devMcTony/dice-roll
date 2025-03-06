dac69c6a9010f933c9bfd665920ec3b1a3cd1bb7
Abel Misiocha <wells.mail.13@gmail.com>
2025-03-06 12:09:39 +0300

    feat(core): Add freq constant

--- dice.ts
+++ dice.ts
@@ -1,5 +1,7 @@
+const FREQUENCY = 1000
+
 function simulateDiceRoll(): void {
-  // Initialize a Map with faces 1 to 6 as keys and empty arrays as values
+  // Initialize the face Map
   const faces: Map<number, number[]> = new Map([
     [1, []],
     [2, []],
@@ -9,11 +11,11 @@ function simulateDiceRoll(): void {
     [6, []],
   ])
 
-  for (let i = 0; i < 1000; i++) {
-    // Generates a number in range [0, 1)
+  for (let i = 0; i < FREQUENCY; i++) {
+    // Generates a number in range btwn [0, 1)
     const num = Math.random()
 
-    // Determine category using range checks (1/6 = ~0.1667)
+    // Determine category by checking (1/6 = ~0.1667)
     let category: number
     if (num < 1 / 6) {
       category = 1
@@ -29,21 +31,21 @@ function simulateDiceRoll(): void {
       category = 6
     }
 
-    // Use Map's get method to access the array and push the number
+    // Get category and push the random number
     faces.get(category)!.push(num)
   }
 
-  // Compute total using Map values
+  // Compute total Frequency using Map values
   const total = Array.from(faces.values()).reduce(
     (sum, arr) => sum + arr.length,
     0,
   )
 
-  // Generate data to output: [{ face: '1', freq: 100, perc: '10.0%' }, ...]
+  // Generate data to output
   const data = Array.from(faces.entries()).map(([face, values]) => {
     const freq = values.length
-    const perc = ((freq / 1000) * 100).toFixed(1) + '%'
-    return { face: face.toString(), freq, perc } // face is a number, convert to string for display
+    const perc = ((freq / FREQUENCY) * 100).toFixed(1) + '%'
+    return { face: face.toString(), freq, perc }
   })
 
   // Output results as table
