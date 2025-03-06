91e4489706db0ccc4ac5b72186785cdea9af6c83
Abel Misiocha <wells.mail.13@gmail.com>
2025-03-06 13:04:41 +0300

    feat(core): Update

--- dice.ts
+++ dice.ts
@@ -12,27 +12,13 @@ function simulateDiceRoll(): void {
   ])
 
   for (let i = 0; i < FREQUENCY; i++) {
-    // Generates a number in range btwn [0, 1)
-    const num = Math.random()
+    // Generates a number in the range inclusive [0, 1)
+    // Math.ramdow() * 6 scales the number generated to the range [0, 6)
+    // Math.floor(Math.random() * 6) generates a number e.g 0,1,2,3,4,5
+    // Add 1 to the generated number to get a number in the range 1,2,3,4,5,6
+    const num = Math.floor(Math.random() * 6) + 1
 
-    // Determine category by checking (1/6 = ~0.1667)
-    let category: number
-    if (num <= 1 / 6) {
-      category = 1
-    } else if (num <= 2 / 6) {
-      category = 2
-    } else if (num <= 3 / 6) {
-      category = 3
-    } else if (num <= 4 / 6) {
-      category = 4
-    } else if (num <= 5 / 6) {
-      category = 5
-    } else {
-      category = 6
-    }
-
-    // Get category and push the random number
-    faces.get(category)!.push(num)
+    faces.get(num)!.push(i)
   }
 
   // Compute total Frequency using Map values
@@ -48,16 +34,17 @@ function simulateDiceRoll(): void {
     return { face: face.toString(), freq, perc }
   })
 
+
   // Output results as table
   console.log(` FACE      | FREQUENCY  | PERCENTAGE `)
-  console.log(`--------------------------------`)
+  console.log(`------------------------------------`)
   // Render data
   data.forEach(({ face, freq, perc }) => {
     console.log(
       ` ${face.padStart(4)}      | ${freq.toString().padStart(9)}  | ${perc.padStart(10)}`,
     )
   })
-  console.log(`--------------------------------`)
+  console.log(`------------------------------------`)
   console.log(` Total     | ${total.toString().padStart(9)}  |  100.0%`)
 }
 
